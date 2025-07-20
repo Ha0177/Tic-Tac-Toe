78abbd22ac23bb0ed8005eac6539e545774d846f
ha0177 <214249506+Ha0177@users.noreply.github.com>
2025-07-20 08:51:20 +0300

    Refactor Gameboard to use an IIFE and add Cell functionality; implement displayController for player management

--- script.js
+++ script.js
@@ -1,15 +1,73 @@
-function Gameboard() {   
+const Gameboard = (function() {   
     const rows = 3;
-    const collumns = 3; 
+    const columns = 3; 
     const board = [];
 
     for (let i = 0; i < rows; i++) { 
         board[i] = []; 
-        for (let j = 0; j < collumns; j++) {
+        for (let j = 0; j < columns; j++) {
             board[i].push(Cell()); 
         }
     }
-    
-    const getBoard = () => board;
+    const getBoard = () => {
+        const boardWithValues = board.map(row => row.map(cell => cell.getValue()));
+        console.log(boardWithValues);
+    };
 
-}
+    const placeMarker = (row, column, playerMarker) => {
+        board[row][column].addMarker(playerMarker);
+    }
+
+
+    return { getBoard, placeMarker };
+})();
+
+function Cell() {
+    let value = ""
+
+    const getValue = () => value;
+
+    const addMarker = (playerMarker) => {
+        value = playerMarker;
+    }
+    
+    return { addMarker, getValue };
+    }
+
+
+const displayController = (function() {
+    playerOneName = "Player One"
+    playerTwoName = "Player Two"
+
+    const board = Gameboard();
+
+    const players = [
+        {
+            name: playerOneName,
+            marker: "X"
+        },
+        {
+            name: playerTwoName,
+            marker: "O"
+        }
+    ];
+
+    let activePlayer = players[0];
+    const switchTurn = () => {
+        activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    };
+    const getActivePlayer = () => activePlayer;
+
+    
+})();
+
+
+
+
+
+
+
+
+
+
+Gameboard.getBoard();
